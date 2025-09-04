5d8057aa921c24590363fb6bc57b7077ea8600a9
Artem Druzhinin <tyomafrutis@mail.ru>
2025-09-04 18:05:31 +0300

    first commit

---
+++ pr 1/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите 1 число:");
+string? num1 = Console.ReadLine();
+Console.WriteLine("Введите операцию:");
+string? operation = Console.ReadLine();
+Console.WriteLine("Введите 2 число:");
+string? num2 = Console.ReadLine();
+Console.WriteLine("Ответ:");
+
+Convert.ToDouble(num1);
+Convert.ToDouble(num2);
+
+switch (operation)
+{
+    case "+":
+        Console.WriteLine(num1 + num2);
+        break;
+
+    default:
+        Console.WriteLine("Некорректный ввод");
+        break;
+}
