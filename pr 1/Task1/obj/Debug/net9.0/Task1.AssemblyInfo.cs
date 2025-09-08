1c625b962b818d888d7d99ffa4c3ccdcd696e48d
Artem Druzhinin <tyomafrutis@mail.ru>
2025-09-08 13:55:51 +0300

    доделал калькулятор + добавил описание

--- pr 1/Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
+++ pr 1/Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6a67adfb4983ad6ada62584b9c644f55a483170")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c79e66984b62ef4a50324b35b182b4f58daebb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
