09ec0d6e5a1ac54c3fef103306b77ccb59484f08
Artem Druzhinin <tyomafrutis@mail.ru>
2025-09-18 19:33:00 +0300

    2.1.3,4

--- pr 2/Task3/Program.cs
+++ pr 2/Task3/Program.cs
@@ -10,6 +10,15 @@ do
     d = Convert.ToInt32(Console.ReadLine());
 } while (d == 0);
 
+// Проверка на знак:
+if (d < 0)
+{
+    n *= -1;
+    d *= -1;
+}
+
+
+// Найдем НОД
 int NOD(int x, int y)
 {
     int temp;
@@ -21,12 +30,12 @@ int NOD(int x, int y)
     }
     return x;
 }
+int nod = NOD(Math.Abs(n), Math.Abs(d));
 
-while (NOD(n, d) != 1)
-{
-    n = n / NOD(n, d);
-    d = d / NOD(n, d);
-}
+n = n / nod;
+d = d / nod;
 
-Console.WriteLine(n);
-Console.WriteLine(d);
+Console.Write("Результат: ");
+Console.Write(n);
+Console.Write(" / ");
+Console.Write(d);
---
+++ pr 2/Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+int left = 1;
+int right = 63;
+string? answer;
+for (int i = 1; i < 7; i++)
+{
+    Console.Write($"Ваше число больше {(left + right) / 2}? (1-да; 0-нет) ");
+    do
+    {
+        answer = Console.ReadLine();
+    } while (answer != "0" && answer != "1");
+
+    left = (answer == "1") ? ((left + right) / 2) + 1 : left;
+    right = (answer == "1") ? right : (left + right) / 2;
+}
+
+Console.WriteLine($"Ответ: {right}");
+
