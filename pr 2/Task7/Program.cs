5a9355ca35f57bfae50249f0a917ddaec106204e
Artem Druzhinin <tyomafrutis@mail.ru>
2025-09-26 20:30:49 +0300

    доделал pr 2 task 7

--- pr 2/Task7/Program.cs
+++ pr 2/Task7/Program.cs
@@ -15,6 +15,24 @@ int h = int.Parse(Console.ReadLine()!);
 
 bool placementFlag(int a, int b, int w, int h, int n)
 {
-    Math.Ceiling(n / (w / a)) <= h
-    return (a * n <= w && b * <= h) || (a <= h && b <= w);
+    int tempX = w / a;
+    if (tempX == 0) return false;
+    int tempY = (n / tempX) * tempX == n ? (n / tempX) : (n / tempX) + 1;
+    return tempY * b <= h;
+    // w / a = сколько поместиться целых штук по одной стороне
+    // (n / (w / a))* (w/a) == n ? (n / (w / a)) : (n / (w / a)) + 1
+    //  Math.Celling(n / (w/a)) * b = какую длину займет расположение
+    // на другой стороне
+    // если верхнее выражение меньше чем h, то поместиться 
+} 
+
+int i = 0;
+int d = 0;
+while (a * b * n <= w * h) {
+    if (placementFlag(a, b, w, h, n) || placementFlag(b, a, w, h, n)) { d = i; }
+    a += 2;
+    b += 2;
+    i++;
 }
+
+Console.WriteLine($"Ответ: d = {d}");
